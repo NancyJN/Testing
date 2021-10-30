68bf615c8e0152350b82e18c97c10f1eeb7279dc
NancyJN <christiannancy555@gmail.com>
2021-10-30 11:12:08 -0400

    changes to string file

--- src/Stringdemo.java
+++ src/Stringdemo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Stringdemo
@@ -6,10 +7,23 @@ public class Stringdemo
     {
 
       String s = "Nancy"; //always contain null address
+        boolean ic = s.contains("an");
+        System.out.println(ic);
+
+        char[] chars = s.toCharArray();
+
+        for (int i =0; i < chars.length ;i++)
+        {
+            System.out.println(chars[i]);
+        }
+        System.out.println(Arrays.toString(chars));
+
         s= s.concat(" Christian");
         s = s.toUpperCase();
         System.out.println(s.length());
         char x = s.charAt(2);
+        String s1 = String.valueOf(x);
+        String s2 = String.valueOf(x);
         System.out.println(x);
         //System.out.println(s.concat("Christian"));
         System.out.println(s);
