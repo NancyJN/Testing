a4300e88a9db364720fa9aac285490dfb1b4136f
NancyJN <christiannancy555@gmail.com>
2021-10-30 10:35:13 -0400

    String demo file

---
+++ src/Stringdemo.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class Stringdemo
+{
+    public static void main(String[] args)
+    {
+
+      String s = "Nancy"; //always contain null address
+        s= s.concat(" Christian");
+        s = s.toUpperCase();
+        //System.out.println(s.concat("Christian"));
+        System.out.println(s);
+
+
+    }
+
+}
