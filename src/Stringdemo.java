13d355644744b293478296b54dafae02f8869fbe
NancyJN <christiannancy555@gmail.com>
2021-10-30 10:53:34 -0400

    changes to char file

--- src/Stringdemo.java
+++ src/Stringdemo.java
@@ -8,6 +8,9 @@ public class Stringdemo
       String s = "Nancy"; //always contain null address
         s= s.concat(" Christian");
         s = s.toUpperCase();
+        System.out.println(s.length());
+        char x = s.charAt(2);
+        System.out.println(x);
         //System.out.println(s.concat("Christian"));
         System.out.println(s);
 
---
+++ src/wrapper.java
@@ -0,0 +1,13 @@
+public class wrapper
+{
+    public static void main(String[] args)
+    {
+
+        Integer aa = 2;
+        int i =aa.intValue();
+        Integer integer = new Integer(i);
+
+
+
+    }
+}
